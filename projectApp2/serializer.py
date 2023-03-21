1395d5d5310387e7b581931cf4b659263675330f
Ajay Kumar Natwariya <anatwariya@thoughtwin.com>
2023-03-21 19:51:09 +0530

    edited serializer for Car Model.

--- projectApp2/serializer.py
+++ projectApp2/serializer.py
@@ -5,10 +5,11 @@ from .models import Car
 
 
 class CarSerializer(serializers.Serializer):
+    depth = 1
     id = serializers.ReadOnlyField()
     model_name = serializers.CharField(max_length=30)
     color = serializers.CharField(max_length=20)
-    parts_available = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    parts_available = serializers.PrimaryKeyRelatedField(many=True, read_only=False, queryset=CarPart.objects.all())
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
@@ -16,5 +17,10 @@ class CarSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.model_name = validated_data["model_name"]
         instance.color = validated_data["color"]
+        print(validated_data)
+        if validated_data.get("parts_available", False):
+            instance.parts_available.clear()
+            for car_part in validated_data['parts_available']:
+                instance.parts_available.add(car_part)
         instance.save()
         return instance
